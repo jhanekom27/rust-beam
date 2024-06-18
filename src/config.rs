e01e3debb8cdd0e58c24ce6b6e5c3aad8648441b
Jurgen <hanekom.jurgen@bcg.com>
2024-06-18 16:07:56 +0800

    chore: tidy code

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 use serde_yaml;
-use std::fs::File;
-use std::io::Read;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
@@ -11,20 +9,9 @@ pub struct Config {
 }
 
 pub fn get_config() -> Config {
-    // let config_path = "config.yaml";
     let config_yaml = include_str!("config.yaml");
     let config: Config =
-        serde_yaml::from_str(config_yaml).expect("Failed to parse YAML");
-    // read_config(config_path).expect("Failed to read configuration file.");
+        serde_yaml::from_str(config_yaml).expect("Failed to parse Config YAML");
     println!("Configuration loaded: {:?}", config);
     config
 }
-
-// fn read_config(file_path: &str) -> Result<Config, serde_yaml::Error> {
-//     let mut file = File::open(file_path).expect("Unable to open file");
-//     let mut contents = String::new();
-//     file.read_to_string(&mut contents)
-//         .expect("Unable to read file");
-//     let config: Config = serde_yaml::from_str(&contents)?;
-//     Ok(config)
-// }

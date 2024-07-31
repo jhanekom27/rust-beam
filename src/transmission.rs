9b3a8f926d5e31941093e218be4d42de0f141405
Jurgen <hanekom.jurgen@bcg.com>
2024-07-31 13:35:42 +0800

    feat: encryption partially working

--- src/transmission.rs
+++ src/transmission.rs
@@ -1,3 +1,5 @@
+use aes_gcm::{aead::Aead, Aes256Gcm, Key, KeyInit, Nonce};
+use rand::{rngs::OsRng, RngCore};
 use std::io;
 use std::pin::Pin;
 
@@ -19,7 +21,7 @@ pub enum Mode {
 }
 
 async fn transfer_bytes_from_source_to_sink(
-    buffer: &mut [u8],
+    mut buffer: &mut [u8],
     source: &mut (dyn tokio::io::AsyncRead + Unpin),
     sink: &mut (dyn tokio::io::AsyncWrite + Unpin),
     progress_tracker: &mut dyn UpdateProgress,
@@ -28,16 +30,52 @@ async fn transfer_bytes_from_source_to_sink(
 ) -> io::Result<()> {
     let mut bytes_read = 0;
 
+    let encryption_key = Key::<Aes256Gcm>::from_slice(key);
+    let mut nonce_bytes = [0u8; 12];
+    let cipher = Aes256Gcm::new(encryption_key);
+
     let mut pinned_source = Pin::new(source);
     let mut pinned_sink = Pin::new(sink);
 
-    while let Ok(n) = pinned_source.as_mut().read(buffer).await {
-        if n == 0 {
-            break;
+    match mode {
+        Mode::Encrypt => {
+            OsRng.fill_bytes(&mut nonce_bytes);
+            let nonce = Nonce::from_slice(&nonce_bytes);
+
+            // Write the nonce to the sink first, so it can be used for decryption later
+            pinned_sink.write_all(&nonce_bytes).await?;
+
+            while let Ok(n) = pinned_source.as_mut().read(&mut buffer).await {
+                if n == 0 {
+                    break;
+                }
+
+                let encrypted_buffer =
+                    cipher.encrypt(nonce, &buffer[..n]).unwrap();
+                pinned_sink.write_all(&encrypted_buffer).await?;
+
+                bytes_read += n;
+                progress_tracker.update_progress(bytes_read as u64);
+            }
         }
-        pinned_sink.as_mut().write_all(&buffer[..n]).await?;
-        bytes_read += n;
-        progress_tracker.update_progress(bytes_read as u64);
+        Mode::Decrypt => {
+            // Read the nonce from the source first
+            pinned_source.read_exact(&mut nonce_bytes).await?;
+            let nonce = Nonce::from_slice(&nonce_bytes);
+
+            while let Ok(n) = pinned_source.as_mut().read(&mut buffer).await {
+                if n == 0 {
+                    break;
+                }
+
+                let decrypted_buffer =
+                    cipher.decrypt(nonce, &buffer[..n]).unwrap();
+                pinned_sink.write_all(&decrypted_buffer).await?;
+
+                bytes_read += n;
+                progress_tracker.update_progress(bytes_read as u64);
+            }
+        }
     }
 
     Ok(())

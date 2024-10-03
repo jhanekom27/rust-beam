ca5a51e8494e10ee5161ababdcd8960525cbf173
Jurgen <hanekom.jurgen@bcg.com>
2024-10-03 14:19:33 +0800

    fix: string formatting with crossterm

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,14 @@
 use copypasta::{ClipboardContext, ClipboardProvider};
-use std::io;
-use std::io::Error;
-use std::io::Write;
-
-// use std::io::{self, Write};
 use crossterm::{
+    cursor::MoveToNextLine,
     event::{self, Event, KeyCode},
-    style::{Color, Print, SetForegroundColor, ResetColor, SetAttribute, Attribute},
+    execute,
+    style::{
+        Attribute, Color, Print, ResetColor, SetAttribute, SetForegroundColor,
+    },
     terminal::{disable_raw_mode, enable_raw_mode},
-    ExecutableCommand,
 };
+use std::io::{self, Error};
 
 use tokio::{io::AsyncReadExt, net::TcpStream};
 
@@ -41,23 +40,17 @@ pub fn copy_key_to_clipbpard(file_key: String) {
     enable_raw_mode().unwrap();
 
     // Output formatted text to the terminal
-    stdout
-        .execute(SetAttribute(Attribute::Bold))
-        .unwrap()
-        .execute(Print(" "))
-        .unwrap()
-        .execute(ResetColor)
-        .unwrap()
-        .execute(Print(format!(
-            " copy key to clipboard: {}{}",
-            SetForegroundColor(Color::Green),
-            file_key
-        )))
-        .unwrap()
-        .execute(ResetColor)
-        .unwrap()
-        .flush()
-        .unwrap();
+    execute!(
+        stdout,
+        Print("copy key to clipboard: "),
+        SetAttribute(Attribute::Bold),
+        SetForegroundColor(Color::Green),
+        Print(file_key.clone()),
+        ResetColor,
+        SetAttribute(Attribute::Reset),
+        MoveToNextLine(1)
+    )
+    .unwrap();
 
     // Listen for key presses
     loop {
@@ -65,7 +58,8 @@ pub fn copy_key_to_clipbpard(file_key: String) {
         if let Event::Key(key_event) = event::read().unwrap() {
             match key_event.code {
                 KeyCode::Char(' ') => {
-                    let mut ctx: ClipboardContext = ClipboardContext::new().unwrap();
+                    let mut ctx: ClipboardContext =
+                        ClipboardContext::new().unwrap();
                     ctx.set_contents(file_key.clone()).unwrap();
                     break;
                 }
@@ -77,35 +71,3 @@ pub fn copy_key_to_clipbpard(file_key: String) {
     // Return stdout to normal mode (disable raw mode)
     disable_raw_mode().unwrap();
 }
-
-// pub fn copy_key_to_clipbpard(file_key: String) {
-//     let mut stdout = io::stdout().into_raw_mode().unwrap();
-//     let stdin = io::stdin();
-
-//     writeln!(
-//         stdout,
-//         "{}<space>{} copy key to clipboard: {}{}{}{}\r",
-//         style::Bold,
-//         style::Reset,
-//         color::Fg(color::Green),
-//         style::Bold,
-//         file_key,
-//         style::Reset
-//     )
-//     .unwrap();
-
-//     for c in stdin.keys() {
-//         match c.unwrap() {
-//             Key::Char(' ') => {
-//                 let mut ctx: ClipboardContext =
-//                     ClipboardContext::new().unwrap();
-//                 ctx.set_contents(file_key.clone()).unwrap();
-//                 break;
-//             }
-//             _ => {}
-//         }
-//     }
-
-//     // return stdout to normal
-//     let _ = stdout.suspend_raw_mode();
-// }
